630d67604ed1237772681607f61f24642a061d43
richard-llmnn <69484873+richard-llmnn@users.noreply.github.com>
2024-01-28 01:04:32 +0100

    add mapbox gl and some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import mapboxgl from 'mapbox-gl';
+	import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
+	import { PUBLIC_MAPBOX_ACCESS_TOKEN, PUBLIC_MAPBOX_STYLE_URL } from '$env/static/public';
+
+	onMount(() => {
+		mapboxgl.accessToken = PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+		const map = new mapboxgl.Map({
+			container: 'map',
+			zoom: 10,
+			style: PUBLIC_MAPBOX_STYLE_URL,
+			center: [12.92137, 50.827847]
+		});
+	});
+</script>
+
+<h1>Supermarktnavigator</h1>
+<div id="map"></div>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+	#map {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		padding: 0;
+		box-sizing: border-box;
+	}
+</style>
